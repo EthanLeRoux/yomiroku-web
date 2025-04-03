23ae04f9dcf40edab57b470c5515a9d970530101
EthanLeRoux <127761539+EthanLeRoux@users.noreply.github.com>
2025-04-03 07:47:02 +0200

    Add react routes for login,signup and home

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import './App.css'
 import Signup from "./components/Signup.jsx";
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Login from "./components/Login.jsx";
+import Home from "./components/Home.jsx";
 
 function App() {
   return (
     <>
-      <Signup></Signup>
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/login" element={<Login />} />
+            </Routes>
+        </Router>
     </>
   )
 }
