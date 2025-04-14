f520b955f32c05eef74131978c65a21e6d46b463
EthanLeRoux <127761539+EthanLeRoux@users.noreply.github.com>
2025-04-14 21:25:08 +0200

    Add index link to logo

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,11 +1,14 @@
 import '/src/assets/css/NavigationBar.css'
+import {Link} from "react-router-dom";
 
 export default function NavigationBar(){
 
     return (
         <>
             <div className={'nav-container'}>
-                <img className={'nav-item'} src={'/src/assets/images/logo.png'}  width={'100px'} height={'100px'} alt={'logo'}/>
+                <Link to={'/'}>
+                    <img className={'nav-item'} src={'/src/assets/images/logo.png'}  width={'100px'} height={'100px'} alt={'logo'}/>
+                </Link>
                 <h1 className={'nav-item'}>yomiroku</h1>
                 <div className={'nav-item-hm'}>
                     <img src={'/src/assets/images/menu.png'} width={'100px'} height={'100px'} alt={'hamburger-menu-icon'} />
