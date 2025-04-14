4b30a640b16fbbdd285212e2a7d6a7e692fdb44b
EthanLeRoux <127761539+EthanLeRoux@users.noreply.github.com>
2025-04-14 21:38:38 +0200

    Remove navbar from home route

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,12 @@
 import AddNewAnimeEntry from "./AddNewAnimeEntry.jsx";
 import NavigationBar from "./NavigationBar.jsx";
+import ImmersionStatistics from "./ImmersionStatistics.jsx";
 
 export default function Home(){
     return (
         <>
-            <NavigationBar/>
-            <AddNewAnimeEntry/>
+
+            <ImmersionStatistics/>
         </>
     )
 }
