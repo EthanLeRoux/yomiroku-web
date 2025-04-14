aab756caf64ce98f17e2fc5d2e5c510afed6eb1d
EthanLeRoux <127761539+EthanLeRoux@users.noreply.github.com>
2025-04-14 21:39:07 +0200

    Add navbar to all routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,19 @@ import Signup from "./components/Signup.jsx";
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Login from "./components/Login.jsx";
 import Home from "./components/Home.jsx";
+import AddNewAnimeEntry from "./components/AddNewAnimeEntry.jsx";
+import NavigationBar from "./components/NavigationBar.jsx";
 
 function App() {
   return (
     <>
         <Router>
+            <NavigationBar/>
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/signup" element={<Signup />} />
                 <Route path="/login" element={<Login />} />
+                <Route path="/newanime" element={<AddNewAnimeEntry/>}/>
             </Routes>
         </Router>
     </>
